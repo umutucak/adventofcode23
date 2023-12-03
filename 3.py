8e1cdec1f12b7e44637b0fef08f20e8b79489a5b
umutucak <umutucak@proton.me>
2023-12-03 15:04:35 +0100

    day3 part1 python

---
+++ 3.py
@@ -0,0 +1,73 @@
+# pyline: disable=consider-using-enumerate
+
+def get_legal_directions(r, c):
+    """look away man
+    
+    please perform an emergency backflip away from your 
+    screen in order to not suffer mental retardation 
+    from looking at this piece of code"""
+    legal = directions.copy()
+    if r == 0:
+        legal.remove((-1, 0))
+        legal.remove((-1, -1))
+        legal.remove((-1, 1))
+    elif r == 139: # 9 for demo, 139 for full
+        legal.remove((1, 0))
+        legal.remove((1, -1))
+        legal.remove((1, 1))
+    if c == 0:
+        legal.remove((0, -1))
+        try: # these may or may not have been removed in the row checks... right...
+            legal.remove((-1, -1))
+        except:
+            pass
+        try:
+            legal.remove((1, -1))
+        except:
+            pass
+    elif c == 139: # 9 for demo, 139 for full
+        legal.remove((0, 1))
+        try: # these may or may not have been removed in the row checks... right...
+            legal.remove((-1, 1))
+        except:
+            pass
+        try:
+            legal.remove((1, 1))
+        except:
+            pass
+    return legal
+
+b = open("3.input", "r").read().split("\n")
+directions = [
+    (-1, 0), # up
+    (1, 0),  # down
+    (0, -1), # left
+    (0, 1),  # right
+    (-1, -1),# up-left
+    (-1, 1), # up-right
+    (1, -1),  # down-left
+    (1, 1),  # down-right
+]
+sum = 0
+part = False
+for r in range(len(b)):
+    b[r] += "."
+    num = ""
+    for c in range(len(b[r])): 
+        entry:str = b[r][c]
+        if not entry.isdigit(): # if not a number move on
+            if part:
+                sum += int(num)
+                part = False
+            num = ""
+            continue
+        num += entry
+        if part: # if already found that it is valid continue reading the rest of the number
+            continue
+        legal_directions = get_legal_directions(r, c)
+        for d in legal_directions: #check 8 directions
+            target = b[r+d[0]][c+d[1]]
+            if target == "." or target.isdigit(): # if not special, continue looking
+                continue
+            part = True # found valid part
+print(sum)
